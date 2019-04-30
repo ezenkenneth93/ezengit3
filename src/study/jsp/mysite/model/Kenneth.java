56782ea3b91a736ebb8b4301800124c319face2d
kenneth <ezenkenneth93@gmail.com>
2019-05-01 08:14:03 +0900

    Kenneth add

---
+++ src/study/jsp/mysite/model/Kenneth.java
@@ -0,0 +1,23 @@
+package study.jsp.mysite.model;
+
+public class Kenneth {
+	private int age;
+	private String name;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Kenneth [age=" + age + ", name=" + name + "]";
+	}
+	
+}
