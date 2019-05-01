3aa97ee5f150c429eb64ad26b9a378ef748d40ee
kenneth93 <ezenkenneth93@gmail.com>
2019-05-01 11:39:15 +0900

    New add

---
+++ src/study/jsp/mysite/model/New.java
@@ -0,0 +1,20 @@
+package study.jsp.mysite.model;
+
+public class New {
+	private String monster;
+
+	public String getMonster() {
+		return monster;
+	}
+
+	public void setMonster(String monster) {
+		this.monster = monster;
+	}
+
+	@Override
+	public String toString() {
+		return "New [monster=" + monster + "]";
+	}
+	
+	
+}
