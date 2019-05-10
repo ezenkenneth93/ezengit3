78152c36c3ed7bd3f321ed886e32b652dfcb4639
speed9103 <speed9103@gmail.com>
2019-05-10 20:38:18 +0900

    이태양이 만든거야

--- src/study/jsp/mysite/model/Member.java
+++ src/study/jsp/mysite/model/Member.java
@@ -2,18 +2,39 @@ package study.jsp.mysite.model;
 
 public class Member {
 	private int MemberNo;
+	private String MemberName;
+	
+
 
 	public int getMemberNo() {
 		return MemberNo;
 	}
 
+
+
 	public void setMemberNo(int memberNo) {
 		MemberNo = memberNo;
 	}
 
+
+
+	public String getMemberName() {
+		return MemberName;
+	}
+
+
+
+	public void setMemberName(String memberName) {
+		MemberName = memberName;
+	}
+
+
+
 	@Override
 	public String toString() {
-		return "Member [MemberNo=" + MemberNo + "]";
+		return "Member [MemberNo=" + MemberNo + ", MemberName=" + MemberName + "]";
 	}
-	
+
+
+
 }
